88e505e66ab2c84ed876cf29a598bd2e4ef5b3f2
susukisunya <aynuj0358@icloud.com>
2026-06-16 21:46:55 +0900

    inital commit

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+//ちゃんとできそうになったら、urlのところはDATABESE_URLに変える。なんか無料で作れそう
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DEV_DATABASE_URL"),
+  },
+});

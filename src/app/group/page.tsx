58d572879387ea6f34e2f63c1075f655b96d9b6b
zero-izayoi <000.reiya.0809@gmail.com>
2026-06-30 23:07:18 +0900

    6/30の最後

---
+++ src/app/group/page.tsx
@@ -0,0 +1,11 @@
+//グループ,ランキングページ
+
+export default function Group() {
+    return (
+        <><div>
+            <h1>グループページ</h1>
+            <p>ここにグループの内容が表示されます。</p>
+        </div>
+        <div>aaa</div></>
+    );
+}

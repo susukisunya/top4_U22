b16f2c38489efd379f0be56d489167b887455584
Gumi1716 <soraqmin2130@icloud.com>
2026-07-07 23:26:09 +0900

    buttonをつくりました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 //ホームページ そら
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>aaaa</div>
+  <Button className="size-32 mx-150 my-10">Button</Button>
   );
 }

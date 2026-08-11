ae57a02150bee28cd1d03bffa0b8a73be9aac58e
Gumi1716 <soraqmin2130@icloud.com>
2026-08-11 16:53:33 +0900

    フッターガワだけ

--- src/components/homepage/Footer.tsx
+++ src/components/homepage/Footer.tsx
@@ -1,1 +1,28 @@
-import { House, } from "lucide-react";
+import { Home, User, Settings,  } from "lucide-react";
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-between items-center border-t-2 px-4 px-3">
+        <div className="flex flex-col items-center">
+            <Home size={22} />
+                <h1 className="text-xs text-gray-600">
+                    ホーム画面
+                </h1>
+        </div>
+
+        <div className="flex flex-col items-center">
+            <User size={22} />
+                <h1 className="text-xs text-gray-600">
+                    グループ
+                </h1>
+        </div>
+
+        <div className="flex flex-col items-center">
+            <Settings size={22} />
+                <h1 className="text-xs text-gray-600">
+                    設定
+                </h1>
+        </div>
+    </footer>
+  );
+}

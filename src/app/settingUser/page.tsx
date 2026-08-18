c5c7b155374db8a4e611d52449ea1cfd64c55c83
Gumi1716 <soraqmin2130@icloud.com>
2026-08-18 17:06:50 +0900

    ユーザー設定をこれから作ります

---
+++ src/app/settingUser/page.tsx
@@ -0,0 +1,10 @@
+import { Avatar } from "@/components/ui/avatar";
+import { Settings } from "lucide-react";
+
+export default function Usersetting(){
+    return(
+        <set className="flex flex-col">
+
+        </set>
+    )
+}

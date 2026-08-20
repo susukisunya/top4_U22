0085e3e5e36dace9985ae60dac758864e50a0284
zero-izayoi <000.reiya.0809@gmail.com>
2026-08-20 15:46:56 +0900

    ヘッターとフッターをつけた

--- src/components/homepage/Header.tsx
+++ src/components/homepage/Header.tsx
@@ -2,7 +2,7 @@ import { Bell, Menu } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between border-b-2 border-black px-4 py-3">
+    <header className="fixed top-0 left-0 z-50 w-full flex items-center justify-between border-b-2 border-black px-4 py-3">
       <Menu size={22} />
 
       <h1 className="text-xl font-bold text-orange-600">

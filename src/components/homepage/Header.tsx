06836525e368750bc426349d1b74be2c608756b3
soramin2 <soramin2130@icloud.com>
2026-08-27 23:53:25 +0900

    ヘッダーの文字更新

--- src/components/homepage/Header.tsx
+++ src/components/homepage/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="fixed top-0 left-0 z-50 w-full flex items-center justify-between  border-b bg-white px-4 py-3 shadow-sm">
       
       <h1 className="text-xl font-bold text-orange-600">
-        遅刻カード
+        遅刻ガード
       </h1>
 
       <Bell size={20} />

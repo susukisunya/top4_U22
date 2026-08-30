6117bf09ade1c5c9a0031d209560f2d8cb896742
soramin2 <soramin2130@icloud.com>
2026-08-30 16:59:23 +0900

    ヘッダーのバグがなくなりました

--- src/components/homepage/Header.tsx
+++ src/components/homepage/Header.tsx
@@ -4,29 +4,29 @@ import { Bell } from "lucide-react";
 import { useEffect, useState } from "react";
 
 type Profile = {
-  username: string;
-  icon: string;
+  name: string;
+  image: string;
 };
 
 export default function Header() {
-  const [username, setName] = useState("");
-  const [icon, setIcon] = useState("");
+  const [name, setName] = useState("");
+  const [image, setIcon] = useState("");
 
   // 呼び出し処理
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await fetch("/api/routes/users");
+      const res = await fetch("/api/users/me");
       const data = await res.json();
 
-      setName(data.username);
-      setIcon(data.icon);
+      setName(data.name);
+      setIcon(data.image);
     };
 
     fetchUser();
   }, []);
 
   // 取得できなかった場合のエラーメッセージ
-  if (!username) {
+  if (!name) {
     return <p>ユーザー情報未登録</p>;
   }
 
@@ -36,14 +36,16 @@ export default function Header() {
 
       <div className="flex items-center gap-3">
         <img
-          src={icon}
-          alt={`${username}のアイコン`}
+          src={image}
+          alt={`${name}のアイコン`}
           className="h-10 w-10 rounded-full object-cover"
         />
 
-        <h1 className="text-xl font-bold text-orange-600">
-          {username}
+        <h1 className="text-s font-bold text-orange-600">
+          {name}
         </h1>
+
+        <h2 className="absolute left-1/2 -translate-x-1/2 text-xl font-bold text-black">遅刻王</h2>
       </div>
 
       <Bell size={20} />

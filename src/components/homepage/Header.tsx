337ad3f8bc5365eeb4776359b1a31e6c7ed43418
soramin2 <soramin2130@icloud.com>
2026-08-30 16:32:33 +0900

    アイコンを表示したかった

--- src/components/homepage/Header.tsx
+++ src/components/homepage/Header.tsx
@@ -10,30 +10,41 @@ type Profile = {
 
 export default function Header() {
   const [username, setName] = useState("");
+  const [icon, setIcon] = useState("");
 
-  //呼び出し処理
+  // 呼び出し処理
   useEffect(() => {
     const fetchUser = async () => {
       const res = await fetch("/api/routes/users");
       const data = await res.json();
 
       setName(data.username);
+      setIcon(data.icon);
     };
 
     fetchUser();
   }, []);
 
-  //取得できなかった場合のエラーメッセージ
+  // 取得できなかった場合のエラーメッセージ
   if (!username) {
-    return <p>ぴーや</p>;
+    return <p>ユーザー情報未登録</p>;
   }
 
-  //表示されるヘッダー
+  // 表示されるヘッダー
   return (
     <header className="fixed top-0 left-0 z-50 w-full flex items-center justify-between border-b bg-white px-4 py-3 shadow-sm">
-      <h1 className="text-xl font-bold text-orange-600">
-        {username}
-      </h1>
+
+      <div className="flex items-center gap-3">
+        <img
+          src={icon}
+          alt={`${username}のアイコン`}
+          className="h-10 w-10 rounded-full object-cover"
+        />
+
+        <h1 className="text-xl font-bold text-orange-600">
+          {username}
+        </h1>
+      </div>
 
       <Bell size={20} />
     </header>
